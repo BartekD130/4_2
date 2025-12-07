04534a03ef9439ab487d2589057bf8f3db517b18
BartekD130 <bartosz.drwal.bd@gmail.com>
2025-12-07 21:43:20 +0000

    Ask user whether they want to add student or see all students

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,34 +17,56 @@ class Main {
       
       //s.addStudent(new Student("Krzysztof", 20));
       //s.addStudent(new Student("Janusz", 40));
-      String name = "";
+
+      // Ask user for two options: 1 - add student, 2 - show students
+      int option = 0;
       while(true){
-           name = System.console().readLine("Wpisz imie studenta: ");
-          if(name.matches("[a-zA-Z]+") && !name.contains(" ")){
+        try{
+           option = Integer.parseInt(System.console().readLine("Wybierz opcje: 1 - dodaj studenta, 2 - wyswietl studentow: "));
+           if(option == 1 || option == 2){
             break;
           }
-          System.out.println("Imie nie moze zawierac cyfr ani spacji");
-      }
-      int age = 0;
-      while(true){
-        try {
-           age = Integer.parseInt(System.console().readLine("Wpisz wiek studenta: "));
-           if(age > 0 && age < 120){
-            break;
-          }
-          System.out.println("Wiek musi byc liczba z przedzialu 1-120");
-        } catch (NumberFormatException e) {
-           System.out.println("Wiek musi byc liczba");
+          System.out.println("Opcja musi byc liczba 1 lub 2");
+        } catch(NumberFormatException e){
+           System.out.println("Opcja musi byc liczba 1 lub 2");
         }
       }
       
-      // int age = Integer.parseInt(System.console().readLine("Wpisz wiek studenta: "));
-      s.addStudent(new Student(name, age));
-      
-      Collection<Student> students = s.getStudents();
-      for(Student current : students) {
-        System.out.println(current.ToString());
+      if (option == 2){
+        Collection<Student> students = s.getStudents();
+        for(Student current : students) {
+          System.out.println(current.ToString());
+        }
+      } else if (option == 1){
+        String name = "";
+        while(true){
+             name = System.console().readLine("Wpisz imie studenta: ");
+            if(name.matches("[a-zA-Z]+") && !name.contains(" ")){
+              break;
+            }
+            System.out.println("Imie nie moze zawierac cyfr ani spacji");
+        }
+        int age = 0;
+        while(true){
+          try {
+             age = Integer.parseInt(System.console().readLine("Wpisz wiek studenta: "));
+             if(age > 0 && age < 120){
+              break;
+            }
+            System.out.println("Wiek musi byc liczba z przedzialu 1-120");
+          } catch (NumberFormatException e) {
+             System.out.println("Wiek musi byc liczba");
+          }
+        }
+
+        // int age = Integer.parseInt(System.console().readLine("Wpisz wiek studenta: "));
+        s.addStudent(new Student(name, age));
       }
+      
+      // Collection<Student> students = s.getStudents();
+      // for(Student current : students) {
+        // System.out.println(current.ToString());
+      // }
     } catch (IOException e) {
 
     }
